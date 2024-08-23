24d08d1fc3307de23a9fb0bc68339c5d1a822f77
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-08-23 18:51:49 -0400

    sign in/ login page added
    - front page styling
    - sign in page and login page using clerk
    - firebase connected
    - generate page started

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth='100vw'>
       <Head>
         <title>Flashcard SaaS</title>
         <meta name='description' content='Create Flashcards from your text.' />
@@ -15,25 +15,80 @@ export default function Home() {
         <Toolbar>
           <Typography variant='h6' style={{flexGrow:1}}>Flashcard</Typography>
           <SignedOut>
-            <Button color='inherit'>Login</Button>
-            <Button color='inherit'>Sign Up</Button>
+            <Button color='inherit' href="/sign-in">Login</Button>
+            <Button color='inherit' href="sign-up">Sign Up</Button>
           </SignedOut>
           <SignedIn>
             <UserButton/>
           </SignedIn>
         </Toolbar>
       </AppBar>
+
       <Box sx={{textAlign:'center', my:4}}>
-        <Typography variant='h3'>Welcome to AI Flashcards</Typography>
-        <Typography variant='h6'>Create flashcards from your text.</Typography>
+        <Typography variant='h3' gutterBottom>Welcome to AI Flashcards</Typography>
+        <Typography variant='h6' gutterBottom>Create flashcards from your text.</Typography>
         <Button variant='contained' color='primary' sx={{mt:2}}>Get Started</Button>
       </Box>
       <Box sx={{my:6}}>
-        <Typography variant="h4" components="h2">Features</Typography>
-        <Grid contained spacing={4}>
+        <Typography variant="h4" gutterBottom>Features</Typography>
+        <Grid container spacing={4}>
           <Grid item xs ={12} md={4}>
-            <Typography variant="h5">Text Input</Typography>
-            <Typography>Input your text and let the AI do the rest.</Typography>
+            <Typography variant="h5" gutterBottom>Text Input</Typography>
+            <Typography>
+              {''}
+              Input your text and let the AI do the rest.
+              </Typography>
+          </Grid>
+          <Grid item xs ={12} md={4}>
+            <Typography variant="h5" gutterBottom>AI Flashcards</Typography>
+            <Typography>
+              {''}
+              Let our AI generate flashcards for you.
+              </Typography>
+          </Grid>
+          <Grid item xs ={12} md={4}>
+            <Typography variant="h5" gutterBottom>Accessible Anywhere</Typography>
+            <Typography>
+              {''}
+              Access your flashcards from any device. Study on the go with ease.
+              </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{my:6, textAlign:'center'}}>
+        <Typography variant='h5' gutterBottom>Pricing</Typography>
+        <Typography variant='h6' gutterBottom>Free for 30 days. No credit card required.</Typography>
+
+        <Grid container spacing={4}>
+          <Grid item xs ={12} md={6}>
+            <Box sx={{p:3, border:'1px solid', borderColor: 'grey.300', borderRadius:2}}>
+              <Typography variant="h5" gutterBottom>Basic</Typography>
+              <Typography variant="h6" gutterBottom>$0.99/month</Typography>
+              <Typography>
+                {''}
+                Access to Basic Flashcards and limited storage.
+              </Typography>
+              <Button variant='contained' color='primary' sx={{mt:2}}>Select Basic Plan</Button>
+            </Box>
+          </Grid>
+          <Grid item xs ={12} md={6}>
+            <Box sx={{p:3, border:'1px solid', borderColor: 'grey.300', borderRadius:2}}>
+              <Typography variant="h5" gutterBottom>Pro</Typography>
+              <Typography variant="h6" gutterBottom>$2.99/month</Typography>
+              <Typography>
+                {''}
+                Unlimited Flashcards and storage.
+              </Typography>
+              <Button variant='contained' color='primary' sx={{mt:2}}>Select Pro Plan</Button>
+            </Box>
+          </Grid>
+          <Grid item xs ={12} md={4}>
+            <Typography variant="h5" gutterBottom>Accessible Anywhere</Typography>
+            <Typography>
+              {''}
+              Access your flashcards from any device. Study on the go with ease.
+              </Typography>
           </Grid>
         </Grid>
       </Box>
