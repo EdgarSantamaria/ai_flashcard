c18c71ade7c1791b65c1b5c10dd6872552e0f806
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-08-27 23:39:28 -0400

    update package.json

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -1,6 +1,7 @@
 import {NextResponse} from 'next/server'
 import Stripe from 'stripe'
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const formatAmountForStripe = (amount) => {
     return Math.round(amount * 100)
